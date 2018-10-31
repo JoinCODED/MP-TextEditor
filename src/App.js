9c7917a8ce8f5201883c085ae7f951fce25082e5
DarthHamza <hamzamakia@gmail.com>
2018-10-31 17:24:17 +0300

    Aziz's super solution

--- src/App.js
+++ src/App.js
@@ -8,21 +8,54 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
+  setStyle(style) {
+    this.setState(prevState => ({ [style]: !prevState[style] }));
+  }
+
+  setColor(color) {
+    this.setState({ color });
+  }
+
+  getStyle() {
+    let style = {
+      color: this.state.color
+    };
+
+    if (this.state.bold) style = { ...style, ...styles.bold };
+    if (this.state.italic) style = { ...style, ...styles.italic };
+    if (this.state.underline) style = { ...style, ...styles.underline };
+
+    return style;
+  }
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          className={`btn ${this.state[style] && "btn-primary"}`}
+          onClick={() => this.setStyle(style)}
+          style={styles[style]}
+          key={style}
+        >
           {style}
         </button>
       );
     });
 
-    let colorBoxes = colors.map(color => {
+    let colorBoxes = this.colors.map(color => {
       return (
         <button
+          onClick={() => this.setColor(color)}
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
         />
@@ -32,7 +65,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea style={this.getStyle()} />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
