569912945879aeb11da2cf2c1f14858e2878c0b6
Lailz <lailaabdulraheem@gmail.com>
2018-10-31 09:57:54 +0300

    States removed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
-import StyleButton from "./StyleButton";
+
+const styles = {
+  bold: { fontWeight: "bold" },
+  italic: { fontStyle: "italic" },
+  underline: { textDecorationLine: "underline" }
+};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      styling: {
-        bold: false,
-        italic: false,
-        underline: false
-      },
-      color: "black"
-    };
-
-    this.colors = ["yellow", "blue", "red", "black", "purple"];
-
-    this.setStyle = this.setStyle.bind(this);
-    this.chooseColor = this.chooseColor.bind(this);
-  }
-
-  setStyle(style) {
-    const styling = { ...this.state.styling };
-    styling[style] = !styling[style];
-    this.setState({ styling: styling });
-  }
-
-  chooseColor(color) {
-    this.setState({ color: color });
-  }
-
   render() {
-    let colorBoxes = this.colors.map(color => {
-      return <button key={color} onClick={() => this.chooseColor(color)} />;
+    let stylings = ["bold", "italic", "underline"];
+    let colors = ["yellow", "blue", "red", "black", "purple"];
+
+    let stylingBoxes = stylings.map(style => {
+      return (
+        <button style={styles[style]} key={style}>
+          {style}
+        </button>
+      );
+    });
+
+    let colorBoxes = colors.map(color => {
+      return (
+        <button
+          style={{ backgroundColor: color, height: 30, width: 30 }}
+          key={color}
+        />
+      );
     });
 
     return (
       <div className="App">
         <br />
-        <StyleButton select={this.setStyle} name="bold" />
-        <StyleButton select={this.setStyle} name="italic" />
-        <StyleButton select={this.setStyle} name="underline" />
+        {stylingBoxes}
         <br />
         <br />
         <textarea />
