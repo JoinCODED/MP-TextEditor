60268f494af2ecd7c6ef3429203e47cf8d704cf9
Lailz <lailaabdulraheem@gmail.com>
2018-11-01 07:00:49 +0300

    Solution fixed

--- src/App.js
+++ src/App.js
@@ -12,25 +12,49 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 class App extends Component {
+  setStyle = style => {
+    const newStyle = !this.state[style];
+    this.setState({ [style]: newStyle });
+  };
+
+  chooseColor = color => this.setState({ color: color });
+
   render() {
-    const stylingBoxes = stylings.map(style => (
-      <button className="btn btn-light" style={styles[style]} key={style}>
+    let stylingBoxes = stylings.map(style => (
+      <button
+        className={this.state[style] ? "btn btn-primary" : "btn btn-light"}
+        style={styles[style]}
+        key={style}
+        onClick={() => this.setStyle(style)}
+      >
         {style}
       </button>
     ));
 
-    const colorBoxes = colors.map(color => (
+    let colorBoxes = colors.map(color => (
       <button
         style={{ backgroundColor: color, height: 30, width: 30 }}
         key={color}
+        onClick={() => this.chooseColor(color)}
       />
     ));
 
     return (
       <div className="App">
-        <div className="my-3">{stylingBoxes}</div>
-        <textarea />
-        <div className="my-3">{colorBoxes}</div>
+        <br />
+        {stylingBoxes}
+        <br />
+        <br />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
+        <br />
+        {colorBoxes}
       </div>
     );
   }
