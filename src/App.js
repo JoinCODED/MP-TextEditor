7a744b8c9d944fc87b473612691dd944e03c93b0
Lailz <lailaabdulraheem@gmail.com>
2018-11-01 07:00:49 +0300

    Solution fixed

--- src/App.js
+++ src/App.js
@@ -12,39 +12,57 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
   setStyle = style => {
     const newStyle = !this.state[style];
     this.setState({ [style]: newStyle });
   };
 
-  chooseColor = color => this.setState({ color: color });
+  chooseColor = color => {
+    this.setState({ color: color });
+  };
 
   render() {
-    let stylingBoxes = stylings.map(style => (
-      <button
-        className={this.state[style] ? "btn btn-primary" : "btn btn-light"}
-        style={styles[style]}
-        key={style}
-        onClick={() => this.setStyle(style)}
-      >
-        {style}
-      </button>
-    ));
+    const styles = {
+      bold: { fontWeight: "bold" },
+      italic: { fontStyle: "italic" },
+      underline: { textDecorationLine: "underline" }
+    };
 
-    let colorBoxes = colors.map(color => (
-      <button
-        style={{ backgroundColor: color, height: 30, width: 30 }}
-        key={color}
-        onClick={() => this.chooseColor(color)}
-      />
-    ));
+    const styleNames = ["bold", "italic", "underline"];
+    const colors = ["yellow", "blue", "red", "black", "purple"];
+
+    const stylingBoxes = styleNames.map(style => {
+      return (
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.setStyle(style)}
+        >
+          {style}
+        </button>
+      );
+    });
+
+    const colorBoxes = colors.map(color => {
+      return (
+        <button
+          style={{ backgroundColor: color, height: 30, width: 30 }}
+          key={color}
+          onClick={() => this.chooseColor(color)}
+        />
+      );
+    });
 
     return (
       <div className="App">
-        <br />
-        {stylingBoxes}
-        <br />
-        <br />
+        <div className="my-3">{stylingBoxes}</div>
         <textarea
           style={{
             color: this.state.color,
@@ -53,8 +71,7 @@ class App extends Component {
             textDecorationLine: this.state.underline ? "underline" : ""
           }}
         />
-        <br />
-        {colorBoxes}
+        <div className="my-3">{colorBoxes}</div>
       </div>
     );
   }
