98cdf35d2827aa56e912675f904246a9d89d89c2
Aziz <alsaff1987@gmail.com>
2019-03-07 16:27:16 +0300

    Rasha's Solution

--- src/App.js
+++ src/App.js
@@ -8,13 +8,40 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: null,
+    fontWeight: null,
+    fontStyle: null,
+    textDecorationLine: null
+  };
+  selectColor = color => {
+    this.setState({ color: color });
+  };
+
+  selectStyle = style => {
+    const translator = {
+      bold: "fontWeight",
+      italic: "fontStyle",
+      underline: "textDeocrationLine"
+    };
+
+    if (this.state[translator[style]])
+      return this.setState({ [translator[style]]: null });
+
+    this.setState(styles[style]);
+  };
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = styleNames.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.selectStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +52,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.selectColor(color)}
         />
       );
     });
@@ -32,7 +60,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea style={this.state} />
+
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
